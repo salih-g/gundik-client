ef3af03e7a1a9e42c164370f2ebde42fc96d8751
Salih Gumus <sudanmerinosu@gmail.com>
2022-02-21 23:13:44 +0300

    move to update

--- src/components/Watch.vue
+++ src/components/Watch.vue
@@ -42,7 +42,8 @@ export default {
 			this.$socket.emit('pause');
 		},
 		async goSecond() {
-			this.player.seekTo(this.moveTo / 60);
+			this.player.seekTo(this.moveTo * 60);
+			this.$socket.emit('moveTo', this.moveTo * 60);
 			// console.log(await this.player.getCurrentTime());
 		},
 	},
